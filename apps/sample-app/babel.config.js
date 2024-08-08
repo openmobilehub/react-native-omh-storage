7f04093bf13f5c92b263915a3e389bbf6ffbe84c
Andrei Zgîrvaci <zgirvaciandrei@gmail.com>
2024-08-08 15:52:27 +0200

    fix: resolve modules correctly in metro (#22)
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

--- apps/sample-app/babel.config.js
+++ apps/sample-app/babel.config.js
@@ -1,19 +1,3 @@
-const path = require('path');
-const fs = require('fs');
-const root = path.resolve(__dirname, '../..');
-
-const alias = {};
-
-const packagesPath = path.join(root, 'packages');
-
-for (const file of fs.readdirSync(packagesPath)) {
-  const packageDirPath = path.join(packagesPath, file);
-  const packageJsonPath = path.join(packageDirPath, 'package.json');
-  const pak = require(packageJsonPath);
-
-  alias[pak.name] = path.join(packageDirPath, pak.source);
-}
-
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
@@ -22,7 +6,6 @@ module.exports = {
       {
         extensions: ['.tsx', '.ts', '.js', '.json'],
         alias: {
-          ...alias,
           '@/components': './src/components',
           '@/constants': './src/constants',
           '@/contexts': './src/contexts',
