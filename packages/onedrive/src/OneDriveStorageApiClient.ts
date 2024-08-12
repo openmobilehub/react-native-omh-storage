0e0737501900c19a1976641aaa47df761149290b
Kamil Kedzierski <kamil.kedzierski94@gmail.com>
2024-08-12 15:51:45 +0200

    chore: resolved pr comment
    Signed-off-by: Kamil Kedzierski <kamil.kedzierski94@gmail.com>

--- packages/onedrive/src/OneDriveStorageApiClient.ts
+++ packages/onedrive/src/OneDriveStorageApiClient.ts
@@ -4,12 +4,12 @@ import Axios, { AxiosError, type AxiosInstance } from 'axios';
 import { BASE_URL } from './data/constants/constants';
 import type { OneDriveErrorResponse } from './data/error/OneDriveErrorResponse';
 
-export class OneDriveStorageApiClient {
+abstract class BaseOneDriveStorageApiClient {
   axiosClient: AxiosInstance;
 
-  constructor() {
+  constructor(baseURL: string) {
     this.axiosClient = Axios.create({
-      baseURL: BASE_URL,
+      baseURL: baseURL,
     });
 
     this.axiosClient.interceptors.response.use(
@@ -29,6 +29,12 @@ export class OneDriveStorageApiClient {
       return error.response.data.error.message;
     }
     return error.message;
+  }
+}
+
+export class OneDriveStorageApiClient extends BaseOneDriveStorageApiClient {
+  constructor() {
+    super(BASE_URL);
   }
 
   setAccessToken(accessToken: string) {
@@ -36,30 +42,8 @@ export class OneDriveStorageApiClient {
   }
 }
 
-export class OneDriveStorageApiClientNoAuth {
-  axiosClient: AxiosInstance;
-
+export class OneDriveStorageApiClientNoAuth extends BaseOneDriveStorageApiClient {
   constructor() {
-    this.axiosClient = Axios.create({
-      baseURL: BASE_URL,
-    });
-
-    this.axiosClient.interceptors.response.use(
-      (response) => response,
-      (error: AxiosError<OneDriveErrorResponse>) => {
-        throw new ApiException(
-          this.getErrorMessage(error),
-          error.response?.status,
-          error
-        );
-      }
-    );
-  }
-
-  private getErrorMessage(error: AxiosError<OneDriveErrorResponse>) {
-    if (error.response?.data?.error?.message) {
-      return error.response.data.error.message;
-    }
-    return error.message;
+    super(BASE_URL);
   }
 }
