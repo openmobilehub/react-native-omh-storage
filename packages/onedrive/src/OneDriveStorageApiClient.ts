fb2fafdaf659c2bc754246f05967a3659b532009
Kamil <kamil.kedzierski@callstack.com>
2024-08-12 16:05:32 +0200

    [OMHD-493] feat: added upload functionality to onedrive package (#28)
    * feat: added upload functionality to onedrive package
    
    Signed-off-by: Kamil Kedzierski <kamil.kedzierski94@gmail.com>
    
    * chore: resolved PR comments
    
    Signed-off-by: Kamil Kedzierski <kamil.kedzierski94@gmail.com>
    
    * chore: resolved pr comment
    
    Signed-off-by: Kamil Kedzierski <kamil.kedzierski94@gmail.com>
    
    ---------
    
    Signed-off-by: Kamil Kedzierski <kamil.kedzierski94@gmail.com>

--- packages/onedrive/src/OneDriveStorageApiClient.ts
+++ packages/onedrive/src/OneDriveStorageApiClient.ts
@@ -4,12 +4,12 @@ import Axios, { AxiosError, type AxiosInstance } from 'axios';
 import { BASE_URL } from './data/constants/constants';
 import type { OneDriveErrorResponse } from './data/error/OneDriveErrorResponse';
 
-export class OneDriveStorageApiClient {
+abstract class BaseOneDriveStorageApiClient {
   axiosClient: AxiosInstance;
 
-  constructor() {
+  constructor(baseURL: string) {
     this.axiosClient = Axios.create({
-      baseURL: BASE_URL,
+      baseURL: baseURL,
     });
 
     this.axiosClient.interceptors.response.use(
@@ -30,8 +30,20 @@ export class OneDriveStorageApiClient {
     }
     return error.message;
   }
+}
+
+export class OneDriveStorageApiClient extends BaseOneDriveStorageApiClient {
+  constructor() {
+    super(BASE_URL);
+  }
 
   setAccessToken(accessToken: string) {
     this.axiosClient.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+  }
+}
+
+export class OneDriveStorageApiClientNoAuth extends BaseOneDriveStorageApiClient {
+  constructor() {
+    super(BASE_URL);
   }
 }
