eaf77188b5e8b36f007a4911d7bdbc07b0070b35
DamianNowak5f <damian.nowak.5f@gmail.com>
2024-08-12 15:59:41 +0200

    fix: revert COCOAPODS downgrade & code cleanup
    Signed-off-by: DamianNowak5f <damian.nowak.5f@gmail.com>

--- apps/sample-app/src/components/bottomSheetContent/content/permissions/list/parts/AccordionText/Accordion.tsx
+++ apps/sample-app/src/components/bottomSheetContent/content/permissions/list/parts/AccordionText/Accordion.tsx
@@ -11,7 +11,7 @@ interface Props {
 const Accordion = ({ title, children }: Props) => {
   const [expanded, setExpanded] = useState(false);
 
-  const handlePress = () => setExpanded(!expanded);
+  const handlePress = () => setExpanded((prevValue) => !prevValue);
 
   return (
     <List.Accordion expanded={expanded} onPress={handlePress} title={title}>
