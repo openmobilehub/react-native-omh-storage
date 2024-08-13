8bd6463c7f20a6d5ec70d692c130b752224e3a38
Maciej Budziński <maciej.budzinski.93@gmail.com>
2024-08-13 08:27:28 +0200

    feat: add onedrive module
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>

---
+++ packages/onedrive/android/src/oldarch/StorageOneDriveModule.kt
@@ -0,0 +1,30 @@
+package com.openmobilehub.reactnative.storage.onedrive
+
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.module.annotations.ReactModule
+
+@ReactModule(name = StorageOneDriveModule.NAME)
+class StorageOneDriveModule(
+  private val reactContext: ReactApplicationContext
+) : ReactContextBaseJavaModule(reactContext) {
+  private val moduleImpl = StorageOneDriveModuleImpl(reactContext)
+
+  @ReactMethod
+  fun initializeStorageClient() {
+    return moduleImpl.initialize()
+  }
+
+  @ReactMethod
+  fun listFiles(folderId: String, promise: Promise) {
+    return moduleImpl.listFiles(folderId, promise)
+  }
+
+  override fun getName() = NAME
+
+  companion object {
+    const val NAME = StorageOneDriveModuleImpl.NAME
+  }
+}
---
+++ packages/onedrive/src/NativeOneDriveStorageClient.ts
@@ -0,0 +1,21 @@
+import type { TurboModule } from 'react-native';
+import { TurboModuleRegistry } from 'react-native';
+
+export type NativeStorageEntity = {
+  type: string;
+  id?: string;
+  name?: string;
+  createdTime?: string;
+  modifiedTime?: string;
+  parentId?: string;
+  mimeType?: string;
+  extension?: string;
+  size?: number;
+};
+
+export interface Spec extends TurboModule {
+  initializeStorageClient(): void;
+  listFiles(folderId: string): Promise<NativeStorageEntity[]>;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>('StorageOneDriveModule');
